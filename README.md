77599e59be36fb1c84624355d8915c5a43a951a4
hyachaguac <hyachaguac@unal.edu.co>
2018-05-09 23:50:39 -0400

    Automatic commit. Done before go to queltrics

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ## webapp_phase1
 
-undefined
+NYU
+
+NY Districts geoshapes 
+https://services5.arcgis.com/GfwWNkhOj9bNBqoJ/arcgis/rest/services/nycd/FeatureServer/0/query?where=1=1&outFields=*&outSR=4326&f=geojson
+
--- index.js
+++ index.js
@@ -1,6 +1,28 @@
+const API_KEY = "AIzaSyARD8AJykZ5pTbZusyz0D-TY1GEJJGYsDY";
 
-(function () {
-    document.getElementById("btn").addEventListener("click", function () {
-        alert("Hi! :)");
-    });
-})();
+var map;
+var NYU_coordenates = {lat: 40.7291, lng: -73.9965};
+var directionsService;
+var directionsRenderer;
+var jsonText = 'https://services5.arcgis.com/GfwWNkhOj9bNBqoJ/arcgis/rest/services/nycd/FeatureServer/0/query?where=1=1&outFields=*&outSR=4326&f=geojson';
+var objJSONText = jsonText;
+
+function initMap(){
+  objJSONText = new google.maps.Map(document.getElementById('map'), {
+    zoom: 10,
+    center: NYU_coordenates
+
+  });
+
+  NYU_coordenates = new google.maps.Marker({
+    position: NYU_coordenates,
+    map: map
+  });
+
+  objJSONText.data.loadGeoJson(
+      'https://services5.arcgis.com/GfwWNkhOj9bNBqoJ/arcgis/rest/services/nycd/FeatureServer/0/query?where=1=1&outFields=*&outSR=4326&f=geojson');
+
+
+  directionsService = new google.maps.DirectionsService();
+  directionsRenderer = new google.maps.DirectionsRenderer();
+}
